e65b97e32f8769433fca8825199a5c78c90677e9
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-02-22 20:44:04 -0600

    added portfolio content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,15 +2,51 @@ export default function MyPortfolio(){
     return(   
        <div>
     
-        <h2> My Contact Info:</h2>
-    
-        <p>If you need to reach me or would like to discuss opportunities:
-            <br></br>
-            E-Mail: cisneros.jason.p@gmail.com <br></br>
-            Github: github.com/jpcisneros411
-        </p>
-    
-        <h4> Thanks for stopping by!</h4>
+        <h2>Projects I've worked on:</h2>
+        <br></br>
+        Click on picture to go to deployed website!
+        <br></br>
+        <h4> Gullivarrs Travels</h4>
+        <a href ="https://github.com/AndiS90/Gullivarrrs-Travels" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://gullivarrrs-travels.herokuapp.com/" target="_blank">
+        <img src="Gullivarrs Travels.png" alt="Gullivarrs Travels" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
+        
+        <h4> Honeymoon Hits List! </h4>
+        <a href ="https://github.com/jpcisneros411/Honeymoon-Hits-List" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://honeymoon-hits-list.herokuapp.com/" target="_blank">
+        <img src="Honeymoon Hits List.png" alt="Honeymoon Hits List" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
+
+        <h4> Simple Notes 4 Me </h4>
+        <a href ="https://github.com/jpcisneros411/simplenotes4me" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://simplenotes4me.herokuapp.com/" target="_blank">
+        <img src="SimpleNotes4Me.png" alt="SimpleNotes4Me" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
+
+        <h4> Tracking Workouts </h4>
+        <a href ="https://github.com/jpcisneros411/trackingworkouts" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://workworkworkout.herokuapp.com/" target="_blank">
+        <img src="FitnessTracker.png" alt="Fitness Tracker" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
+
+        <h4> Budget-Tracker </h4>
+        <a href ="https://github.com/jpcisneros411/budget-tracker" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://workworkworkout.herokuapp.com/" target="_blank">
+        <img src="BudgetTracker.png" alt="Budget Tracker" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
+
+        <h4> Coding Quiz teehee </h4>
+        <a href ="https://github.com/jpcisneros411/assignment4jc" target="_blank">Github Repo</a>
+        <br></br>
+        <a href = "https://jpcisneros411.github.io/assignment4jc/" target="_blank">
+        <img src="CodingQuiz.png" alt="Coding Quiz" target="_blank" height="250px;" width="400px;"></img>  
+        </a>
         </div>
     );
     }
